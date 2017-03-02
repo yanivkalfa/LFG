79f3e1fa4bab0409d5c5982bed723b9481e1954b
yaniv kalfa <netameta1@yahoo.com>
2017-03-02 03:11:03 -0500

    scroll buttons now have entire event not just index

--- Components/LFG_EventDropDown.lua
+++ Components/LFG_EventDropDown.lua
@@ -1,26 +1,24 @@
 LFG.EventDropDownMenu = {};
 
 function LFG.EventDropDownMenu.OnClick()
-  --DEFAULT_CHAT_FRAME:AddMessage("UIDROPDOWNMENU_MENU_VALUE".. UIDROPDOWNMENU_MENU_VALUE);
 	local id = this:GetID();
   local name = LFG.EventScrollFrames.eventDropDownFocus.name;
-  local index = LFG.EventScrollFrames.eventDropDownFocus.index;
+  local event = LFG.EventScrollFrames.eventDropDownFocus.event;
   
 	if ( id == 1 ) then
     if(this.arg1 == "signup") then
-      LFG.RolePicker.Show(name, index);
+      LFG.RolePicker.Show(name, event);
       
     elseif(this.arg1 == "cancel") then 
-      LFG.Actions.Queue.cancel(name, index);
+      LFG.Actions.Queue.cancel(name, event);
     end
   end
 end
 
 function LFG.EventDropDownMenu.Initialize()
-	local index = LFG.EventScrollFrames.eventDropDownFocus.index;
-  local event = LFG.EventScrollFrames.eventList[index];
-  
-	local info = {};
+  local event = LFG.EventScrollFrames.eventDropDownFocus.event;
+
+  local info = {};
   info.func = LFG.EventDropDownMenu.OnClick;
   if(not event or not event.QTE) then
     info.text = "Sign Up";
